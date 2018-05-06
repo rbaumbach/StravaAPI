9becd140430a58ccaf3c0cdcc2a435f7eca38eca
Ryan <github@ryan.codes>
2018-05-06 09:59:43 -0600

    Add note about lack of avatarURL

--- StravaAPI/AppCode/Models/Athlete.h
+++ StravaAPI/AppCode/Models/Athlete.h
@@ -2,6 +2,7 @@
 
 @interface Athlete : NSObject
 
+@property (copy, nonatomic) NSURL *avatarURL;
 @property (copy, nonatomic) NSString *firstName;
 @property (copy, nonatomic) NSString *lastName;
 @property (copy, nonatomic) NSString *email;
--- StravaAPI/Networking/AthleteDeserializer.m
+++ StravaAPI/Networking/AthleteDeserializer.m
@@ -6,6 +6,10 @@
 - (Athlete *)deserialize:(NSDictionary *)response {
     Athlete *athlete = [[Athlete alloc] init];
     
+    // Note: avatarURL for the athlete is ignored for now since the URL given back from the athlete
+    //       endpoint is not the full URL.  I'm assuming this is because I'm using token athentication
+    //       and public profile images shouldn't be given so easily.
+    
     athlete.firstName = response[@"firstname"];
     athlete.lastName = response[@"lastname"];
     athlete.email = response[@"email"];
